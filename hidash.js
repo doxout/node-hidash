b6812be124e122fa68aadddc2a2c8c88f02a4bc3
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-08-15 13:42:18 +0200

    minor fixups in doc

--- hidash.js
+++ hidash.js
@@ -1,45 +1,58 @@
-var _ = module.exports = require('lodash');
+/**
+ * # Hidash 
+ * A mixin extension of lodash
+ *
+ */
 
+var _ = module.exports = require('lodash');
 var self = {};
 
 /**
  * Filter and map at the same time
+ * 
+ * Example: 
+ *
+ *      _.filtermap([1,2,3], function(x) { if (x > 1) return x * x; });
+ *
  * @type {Function}
- * @param arr {Array} The array to filter-map
- * @param f - {Function} (item, k) => result-or-undefined
- *      If the function returns undefined, the item will be filtered out.
- * @return the resulting lodash array
+ * @param {Array} array - The array to filter-map
+ * @param {Function} f - function(item, k) => result or undefined
+ *        If the function returns undefined, the item will be filtered out.
+ * @return {Array} the resulting lodash array
  */
-var filtermap = self.filtermap = function filmap(arr, f) {
-    return _.reduce(arr, function(acc, el, k) {
+
+function filtermap(array, f) {
+    return _.reduce(array, function(acc, el, k) {
         var res = f(el, k);
         if (res !== undefined) 
             acc.push(res);
         return acc;
     }, []);
-};
+}
+self.filtermap = filtermap;
+
 
 /**
  * Get a join product of two arrays
+ * 
  * @type {Function}
- * @param arr {Array} The first array
- * @param other {Array} The other array
- * @param f {Function} -> (item1, item2) => result
- *      If the function returns undefined, the pair will be filtered out.
+ * @param {Array} array - The first array
+ * @param {Array} other - The other array
+ * @param {Function} f - (item1, item2) => result. If the function returns undefined, the pair will be filtered out.
+ * @return {Array} - the resulting product.
  */
-self.product = product;
-function product(arr, other, f) {
+function product(array, other, f) {
 
-    if (!f) f = function() { return true; }
+    if (!f) f = function(x, y) { return [x, y]; }
     if (other.filtermap) // unpack
         other = other.value();
 
-    return _.reduce(arr, function reductor(acc, el1) {
+    return _.reduce(array, function reductor(acc, el1) {
         function f2(el2) { return f(el1, el2); }
         return acc.concat(_.filtermap(other, f2));
     }, []);
 }
-
+self.product = product;
 
 
 function stringSelector(str) {
@@ -53,15 +66,14 @@ function identity(x) { return x; }
 
 /**
  * Convert an array of items to a dictionary.
+ * 
  * @type {Function}
- * @param arr {Array} the array to convert
- * @param keyselector Either function (item, k) => key or just property name
- * @param valselector
- *      Either function (item, k) => key, property name or undefined
- *      If not specified, the array item is used.
- * @returns {*} A dictionary.
+ * @param {Array} array - The array to convert
+ * @param {Function,String} keyselector - Either function (item, k) => key or just property name
+ * @param {Function,String} valselector - Either function (item, k) => key, property name or undefined. If not specified, the array item is used.
+ * @return {*} A dictionary.
  */
-function toDictionary(arr, keyselector, valselector) {
+function toDictionary(array, keyselector, valselector) {
     var o = {};
     if (typeof(keyselector) == 'string')
         keyselector = stringSelector(keyselector);
@@ -69,12 +81,11 @@ function toDictionary(arr, keyselector, valselector) {
         valselector = stringSelector(valselector);
     if (!valselector) 
         valselector = identity;
-    return _(arr).reduce(function(acc, el, k) {
+    return _(array).reduce(function(acc, el, k) {
         acc[keyselector(el, k)] = valselector(el, k);
         return acc;
     }, {});
 }
-
 self.toDictionary = self.toObject = toDictionary;
 
 _.mixin(self);
